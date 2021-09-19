88b296947d1a81871623130e4ba8206f5bbc982a
kuroyuma <k.1993.yuma.0324@gmail.com>
2021-09-19 12:44:56 +0000

    [２1]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
 
   root to: 'homes#top'
+  get "home/about" => "home/about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
