f08700c2c757745f999773ef98afae32e16fe17f
kuroyuma <k.1993.yuma.0324@gmail.com>
2021-09-19 09:05:00 +0000

    [２０]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resource :users, only: [:new, :create, :index, :show, :edit, :destroy]
-  resource :books, only: [:new, :create, :index, :show, :edit, :destroy]
   devise_for :users
+
+  resources :users, only: [:new, :create, :index, :show, :edit, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
+
   root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
