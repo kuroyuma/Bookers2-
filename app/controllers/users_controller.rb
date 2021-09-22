b2a39539337934dbe708843d40ca739f33a74a92
kuroyuma <k.1993.yuma.0324@gmail.com>
2021-09-22 14:20:37 +0000

    [4８]4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,8 +23,11 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id), notice: 'You have successfully changed your profile！！'
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: 'You have successfully changed your profile！！'
+    else
+      render :edit
+    end
   end
 
 
